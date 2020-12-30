978e05de52ca0e99a05283484476498d8e86c6e0
Anand prabhakar <anandprabhakar0507@gmail.com>
2020-12-30 15:11:57 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+}, false);
+
+
+function gofullscreen(){
+    if (document.fullscreenEnabled || document.mozRequestFullScreen || document.webkitRequestFullscreen || document.msRequestFullscreen){
+        // fullscreen is activated
+        document.documentElement.requestFullscreen();
+        document.documentElement.mozRequestFullScreen();
+        document.documentElement.webkitRequestFullscreen();
+        document.documentElement.msRequestFullscreen();
+        
+    } else{
+        // fullscreen is cancelled
+        document.exitFullscreen();
+    }
+}
+
+
+function knockit(){
+    if (document.fullscreenEnabled) {
+        console.log("going to Fullscreen mode");
+        document.getElementById('container').requestFullscreen();
+        
+    }
+}
+
+function gototop() {
+    
+    window.onscroll = function() {scrollFunction()};                   
+    function scrollFunction() {
+        if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+            document.getElementById("top").style.display = "block";
+        }else {
+        document.getElementById("top").style.display = "none";
+    }
+
+
+}
+
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
