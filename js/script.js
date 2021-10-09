597920b9e04691979af816bd764ce9ac90bb4af0
Restyled.io <commits@restyled.io>
2021-10-09 16:58:22 +0000

    Restyled by clang-format

--- js/script.js
+++ js/script.js
@@ -1,48 +1,44 @@
 
 
+document.addEventListener("contextmenu", function(e) { e.preventDefault(); },
+                          false);
 
-document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-}, false);
+function gofullscreen() {
+  if (document.fullscreenEnabled || document.mozRequestFullScreen ||
+      document.webkitRequestFullscreen || document.msRequestFullscreen) {
+    // fullscreen is activated
+    document.documentElement.requestFullscreen();
+    document.documentElement.mozRequestFullScreen();
+    document.documentElement.webkitRequestFullscreen();
+    document.documentElement.msRequestFullscreen();
 
-
-function gofullscreen(){
-    if (document.fullscreenEnabled || document.mozRequestFullScreen || document.webkitRequestFullscreen || document.msRequestFullscreen){
-        // fullscreen is activated
-        document.documentElement.requestFullscreen();
-        document.documentElement.mozRequestFullScreen();
-        document.documentElement.webkitRequestFullscreen();
-        document.documentElement.msRequestFullscreen();
-        
-    } else{
-        // fullscreen is cancelled
-        document.exitFullscreen();
-    }
+  } else {
+    // fullscreen is cancelled
+    document.exitFullscreen();
+  }
 }
 
-
-function knockit(){
-    if (document.fullscreenEnabled) {
-        console.log("going to Fullscreen mode");
-        document.getElementById('container').requestFullscreen();
-        document.getElementById('container').mozRequestFullScreen();
-        document.getElementById('container').webKitRequestFullScreen();
-    }
+function knockit() {
+  if (document.fullscreenEnabled) {
+    console.log("going to Fullscreen mode");
+    document.getElementById('container').requestFullscreen();
+    document.getElementById('container').mozRequestFullScreen();
+    document.getElementById('container').webKitRequestFullScreen();
+  }
 }
 
 function gototop() {
-    
-    window.onscroll = function() {scrollFunction()};                   
-    function scrollFunction() {
-        if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
-            document.getElementById("top").style.display = "block";
-        }else {
-        document.getElementById("top").style.display = "none";
+
+  window.onscroll = function() { scrollFunction() };
+  function scrollFunction() {
+    if (document.body.scrollTop > 15 ||
+        document.documentElement.scrollTop > 15) {
+      document.getElementById("top").style.display = "block";
+    } else {
+      document.getElementById("top").style.display = "none";
     }
+  }
 
-
-}
-
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
