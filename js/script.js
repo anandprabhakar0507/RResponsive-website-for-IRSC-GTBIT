795677a88728c3c5fff741ba64cafb87c0ac0b30
Restyled.io <commits@restyled.io>
2021-10-09 16:58:24 +0000

    Restyled by prettier

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-
-
-document.addEventListener("contextmenu", function(e) { e.preventDefault(); },
-                          false);
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+  },
+  false
+);
 
 function gofullscreen() {
-  if (document.fullscreenEnabled || document.mozRequestFullScreen ||
-      document.webkitRequestFullscreen || document.msRequestFullscreen) {
+  if (
+    document.fullscreenEnabled ||
+    document.mozRequestFullScreen ||
+    document.webkitRequestFullscreen ||
+    document.msRequestFullscreen
+  ) {
     // fullscreen is activated
     document.documentElement.requestFullscreen();
     document.documentElement.mozRequestFullScreen();
     document.documentElement.webkitRequestFullscreen();
     document.documentElement.msRequestFullscreen();
-
   } else {
     // fullscreen is cancelled
     document.exitFullscreen();
@@ -21,18 +27,21 @@ function gofullscreen() {
 function knockit() {
   if (document.fullscreenEnabled) {
     console.log("going to Fullscreen mode");
-    document.getElementById('container').requestFullscreen();
-    document.getElementById('container').mozRequestFullScreen();
-    document.getElementById('container').webKitRequestFullScreen();
+    document.getElementById("container").requestFullscreen();
+    document.getElementById("container").mozRequestFullScreen();
+    document.getElementById("container").webKitRequestFullScreen();
   }
 }
 
 function gototop() {
-
-  window.onscroll = function() { scrollFunction() };
+  window.onscroll = function () {
+    scrollFunction();
+  };
   function scrollFunction() {
-    if (document.body.scrollTop > 15 ||
-        document.documentElement.scrollTop > 15) {
+    if (
+      document.body.scrollTop > 15 ||
+      document.documentElement.scrollTop > 15
+    ) {
       document.getElementById("top").style.display = "block";
     } else {
       document.getElementById("top").style.display = "none";
